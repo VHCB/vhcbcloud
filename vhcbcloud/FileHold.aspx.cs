a7a679283a53b41bb7d6cf784e87bc23790c42d5
mlakinana <manoj.lakinana@yahoo.com>
2015-11-24 00:32:39 -0800

    comitted more code

--- vhcbcloud/FileHold.aspx.cs
+++ vhcbcloud/FileHold.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,7 +12,19 @@ namespace vhcbcloud
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
+            if (!IsPostBack)
+            {
+                string url = "http://192.168.100.13/FH/FileHold/WebClient/LoginForm.aspx";
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<script type = 'text/javascript'>");
+                sb.Append("window.open('");
+                sb.Append(url);
+                sb.Append("');");
+                sb.Append("</script>");
+                ClientScript.RegisterStartupScript(this.GetType(),
+                        "script", sb.ToString());
+            }
         }
     }
 }
