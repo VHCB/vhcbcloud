a2773bcbf2164c977f00274a5a2ed738763b9832
mlakinana <manoj.lakinana@yahoo.com>
2016-02-24 23:57:19 -0800

    Lot of fixes applied to site

--- vhcbcloud/Account/ManageUserInfo.aspx.designer.cs
+++ vhcbcloud/Account/ManageUserInfo.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace vhcbcloud.Account {
         protected global::System.Web.UI.WebControls.TextBox txtCPassword;
         
         /// <summary>
+        /// lblErrorMsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErrorMsg;
+        
+        /// <summary>
         /// btnUserInfoSubmit control.
         /// </summary>
         /// <remarks>
