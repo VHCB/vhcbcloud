ea9b38ac8209ed9737a719a3556c3fdce03e2f99
ramakrishna-namburu <ramakrishna.namburu@outlook.com>
2017-05-15 22:27:53 -0700

    Enterprise Grant Implemented

---
+++ vhcb-bal/Viability/EnterpriseImpGrantData.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VHCBCommon.DataAccessLayer.Viability
+{
+    public class EnterpriseImpGrantData
+    {
+        public static DataRow GetEnterpriseImpGrantsById(int ProjectID)
+        {
+            DataRow dr = null;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "GetEnterpriseImpGrantsById";
+                        command.Parameters.Add(new SqlParameter("ProjectID", ProjectID));
+
+                        DataSet ds = new DataSet();
+                        var da = new SqlDataAdapter(command);
+                        da.Fill(ds);
+                        if (ds.Tables.Count == 1 && ds.Tables[0].Rows.Count > 0)
+                        {
+                            dr = ds.Tables[0].Rows[0];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dr;
+        }
+
+        public static ViabilityMaintResult AddEnterpriseImpGrants(int ProjectID, int FYGrantRound, string ProjTitle, string ProjDesc, 
+            decimal ProjCost, decimal Request, decimal AwardAmt, string AwardDesc, decimal LeveragedFunds, string Comments)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "AddEnterpriseImpGrants";
+
+                        command.Parameters.Add(new SqlParameter("ProjectID", ProjectID));
+                        command.Parameters.Add(new SqlParameter("FYGrantRound", FYGrantRound));
+                        command.Parameters.Add(new SqlParameter("ProjTitle", ProjTitle));
+                        command.Parameters.Add(new SqlParameter("ProjDesc", ProjDesc));
+                        command.Parameters.Add(new SqlParameter("ProjCost", ProjCost));
+                        command.Parameters.Add(new SqlParameter("Request", Request));
+                        command.Parameters.Add(new SqlParameter("AwardAmt", AwardAmt));
+                        command.Parameters.Add(new SqlParameter("AwardDesc", AwardDesc));
+                        command.Parameters.Add(new SqlParameter("LeveragedFunds", LeveragedFunds));
+                        command.Parameters.Add(new SqlParameter("Comments", Comments));
+                        
+                        SqlParameter parmMessage = new SqlParameter("@isDuplicate", SqlDbType.Bit);
+                        parmMessage.Direction = ParameterDirection.Output;
+                        command.Parameters.Add(parmMessage);
+
+                        SqlParameter parmMessage1 = new SqlParameter("@isActive", SqlDbType.Int);
+                        parmMessage1.Direction = ParameterDirection.Output;
+                        command.Parameters.Add(parmMessage1);
+
+                        command.ExecuteNonQuery();
+
+                        ViabilityMaintResult objResult = new ViabilityMaintResult();
+
+                        objResult.IsDuplicate = DataUtils.GetBool(command.Parameters["@isDuplicate"].Value.ToString());
+                        objResult.IsActive = DataUtils.GetBool(command.Parameters["@isActive"].Value.ToString());
+
+                        return objResult;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void UpdateEnterpriseImpGrants(int EnterImpGrantID, int FYGrantRound, string ProjTitle, string ProjDesc,
+            decimal ProjCost, decimal Request, decimal AwardAmt, string AwardDesc, decimal LeveragedFunds, string Comments, bool RowIsActive)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "UpdateEnterpriseImpGrants";
+
+                        command.Parameters.Add(new SqlParameter("EnterImpGrantID", EnterImpGrantID));
+                        command.Parameters.Add(new SqlParameter("FYGrantRound", FYGrantRound));
+                        command.Parameters.Add(new SqlParameter("ProjTitle", ProjTitle));
+                        command.Parameters.Add(new SqlParameter("ProjDesc", ProjDesc));
+                        command.Parameters.Add(new SqlParameter("ProjCost", ProjCost));
+                        command.Parameters.Add(new SqlParameter("Request", Request));
+                        command.Parameters.Add(new SqlParameter("AwardAmt", AwardAmt));
+                        command.Parameters.Add(new SqlParameter("AwardDesc", AwardDesc));
+                        command.Parameters.Add(new SqlParameter("LeveragedFunds", LeveragedFunds));
+                        command.Parameters.Add(new SqlParameter("Comments", Comments));
+                        command.Parameters.Add(new SqlParameter("RowIsActive", RowIsActive));
+
+                        command.CommandTimeout = 60 * 5;
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static DataTable GetEnterpriseGrantMatchList(int EnterImpGrantID, bool IsActiveOnly)
+        {
+            DataTable dt = null;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "GetEnterpriseGrantMatchList";
+                        command.Parameters.Add(new SqlParameter("EnterImpGrantID", EnterImpGrantID));
+                        command.Parameters.Add(new SqlParameter("IsActiveOnly", IsActiveOnly));
+
+                        DataSet ds = new DataSet();
+                        var da = new SqlDataAdapter(command);
+                        da.Fill(ds);
+                        if (ds.Tables.Count == 1 && ds.Tables[0].Rows != null)
+                        {
+                            dt = ds.Tables[0];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dt;
+        }
+
+        public static void UpdateEnterpriseGrantMatch(int EnterpriseGrantMatchID, bool RowIsActive)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "UpdateEnterpriseGrantMatch";
+
+                        //4 Parameters
+                        command.Parameters.Add(new SqlParameter("EnterpriseGrantMatchID", EnterpriseGrantMatchID));
+                        command.Parameters.Add(new SqlParameter("RowIsActive", RowIsActive));
+
+                        command.CommandTimeout = 60 * 5;
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static ViabilityMaintResult AddEnterpriseGrantMatch(int EnterImpGrantID, int MatchDescID)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = connection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "AddEnterpriseGrantMatch";
+
+                        command.Parameters.Add(new SqlParameter("EnterImpGrantID", EnterImpGrantID));
+                        command.Parameters.Add(new SqlParameter("MatchDescID", MatchDescID));
+
+                        SqlParameter parmMessage = new SqlParameter("@isDuplicate", SqlDbType.Bit);
+                        parmMessage.Direction = ParameterDirection.Output;
+                        command.Parameters.Add(parmMessage);
+
+                        SqlParameter parmMessage1 = new SqlParameter("@isActive", SqlDbType.Int);
+                        parmMessage1.Direction = ParameterDirection.Output;
+                        command.Parameters.Add(parmMessage1);
+
+                        command.ExecuteNonQuery();
+
+                        ViabilityMaintResult objResult = new ViabilityMaintResult();
+
+                        objResult.IsDuplicate = DataUtils.GetBool(command.Parameters["@isDuplicate"].Value.ToString());
+                        objResult.IsActive = DataUtils.GetBool(command.Parameters["@isActive"].Value.ToString());
+
+                        return objResult;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
