91bfd87e9902de1af482def837762408164243b8
mlakinana <manoj.lakinana@yahoo.com>
2017-05-27 17:17:31 -0700

    New task list items checked in progress captured int the task list

--- vhcbcloud/SQL/FinancialTransactionDetails.sql
+++ vhcbcloud/SQL/FinancialTransactionDetails.sql
@@ -984,45 +984,72 @@ alter procedure GetFinancialTransactionDetails
 	
 )
 as
---exec GetFinancialTransactionDetails 6622, 238, '05/01/2016', '05/16/2016' 
+--exec GetFinancialTransactionDetails 6637, 238, '05/08/2017', '05/16/2017' 
 --exec GetFinancialTransactionDetails 5615, 239, '01/01/2016', '02/01/2016'--DeCommit
 --exec GetFinancialTransactionDetails 5615, -1, '01/01/2016', '02/01/2016'--All
 --exec GetFinancialTransactionDetails -1, -1, '01/01/2016', '02/07/2016'--All
 begin
 
-	select trans.TransId, pv.project_name ProjectName, pv.proj_num ProjectNumber, trans.Date as TransactionDate, trans.TransAmt, v.description as LkTransactionDesc--, trans.LkTransaction, v.*
-	from Trans trans(nolock)
-	left join project_v  pv(nolock) on pv.project_id = trans.ProjectID
-	left join TransAction_v v(nolock) on v.typeid = trans.LkTransaction
-	where trans.TransId in (
-		select t.TransId from (
-		select trans.TransId as TransId, trans.TransAmt,
-				sum(det.Amount) amount, trans.TransAmt - sum(det.Amount) bal
-			from Trans trans(nolock)
-				join detail det(nolock) on trans.TransId = det.TransId
-			where trans.Date >= @tran_start_date 
-				and trans. Date <= @tran_end_date 
-				and trans.LKStatus = 261 and trans.RowIsActive=1  
-								and (trans.projectid = @project_id or (@project_id = -1 and trans.projectid is not null))
-				and (trans.LkTransaction = @financial_transaction_action_id or (@financial_transaction_action_id = -1 and trans.LkTransaction is not null))
-		group by trans.TransId, trans.TransAmt)t
-		where t.bal = 0 and pv.defname = 1  
-	) order by pv.proj_num
+	if ( @financial_transaction_action_id = 236)
+	begin
+		select trans.TransId, pv.project_name ProjectName, pv.proj_num ProjectNumber, trans.Date as TransactionDate, trans.TransAmt, v.description as LkTransactionDesc--, trans.LkTransaction, v.*
+		from Trans trans(nolock)
+		left join project_v  pv(nolock) on pv.project_id = trans.ProjectID
+		left join TransAction_v v(nolock) on v.typeid = trans.LkTransaction
+		where trans.TransId in (
+			select t.TransId from (
+			select case when ((select count(*) from ProjectCheckReqQuestions where ProjectCheckReqID = pcr.ProjectCheckReqID and Approved =0)>0) then 
+			 0 else  trans.TransId end as TransId
+			 , trans.TransAmt
+			 , sum(det.Amount) amount, trans.TransAmt - sum(det.Amount) bal
+				from Trans trans(nolock)
+					join detail det(nolock) on trans.TransId = det.TransId
+					 join ProjectCheckReq pcr on pcr.ProjectCheckReqID = trans.ProjectCheckReqID					 
+				where pcr.CRDate >= @tran_start_date 
+					and pcr.CRDate <= @tran_end_date 
+					and trans.LKStatus = 261 and trans.RowIsActive=1  
+									and (trans.projectid = @project_id or (@project_id = -1 and trans.projectid is not null))
+					and (trans.LkTransaction = @financial_transaction_action_id or (@financial_transaction_action_id = -1 and trans.LkTransaction is not null))
+					
+			group by trans.TransId, trans.TransAmt, pcr.projectcheckreqid)t
+			where t.bal = 0 and pv.defname = 1  
+		) order by pv.proj_num
+	end
+	else 
+	begin
+		select trans.TransId, pv.project_name ProjectName, pv.proj_num ProjectNumber, trans.Date as TransactionDate, trans.TransAmt, v.description as LkTransactionDesc--, trans.LkTransaction, v.*
+		from Trans trans(nolock)
+		left join project_v  pv(nolock) on pv.project_id = trans.ProjectID
+		left join TransAction_v v(nolock) on v.typeid = trans.LkTransaction
+		where trans.TransId in (
+			select t.TransId from (
+			select trans.TransId as TransId, trans.TransAmt,
+					sum(det.Amount) amount, trans.TransAmt - sum(det.Amount) bal
+				from Trans trans(nolock)
+					join detail det(nolock) on trans.TransId = det.TransId
+				where trans.Date >= @tran_start_date 
+					and trans. Date <= @tran_end_date 
+					and trans.LKStatus = 261 and trans.RowIsActive=1  
+									and (trans.projectid = @project_id or (@project_id = -1 and trans.projectid is not null))
+					and (trans.LkTransaction = @financial_transaction_action_id or (@financial_transaction_action_id = -1 and trans.LkTransaction is not null))
+			group by trans.TransId, trans.TransAmt)t
+			where t.bal = 0 and pv.defname = 1  
+		) order by pv.proj_num
 
-	--select t.TransId from (
-	--select trans.TransId as TransId, trans.TransAmt,
-	--			sum(det.Amount) amount, trans.TransAmt - sum(det.Amount) bal
-	--		from Trans trans(nolock)
-	--			join detail det(nolock) on trans.TransId = det.TransId
-	--		where trans.Date >= '01/01/2016' 
-	--			and trans. Date <= '02/07/2016'  
-	--			and trans.LKStatus = 261
-	--			and trans.projectid is not null
-	--			and trans.LkTransaction is not null
-	--	group by trans.TransId, trans.TransAmt)t
-	--	where t.bal = 0
+		--select t.TransId from (
+		--select trans.TransId as TransId, trans.TransAmt,
+		--			sum(det.Amount) amount, trans.TransAmt - sum(det.Amount) bal
+		--		from Trans trans(nolock)
+		--			join detail det(nolock) on trans.TransId = det.TransId
+		--		where trans.Date >= '01/01/2016' 
+		--			and trans. Date <= '02/07/2016'  
+		--			and trans.LKStatus = 261
+		--			and trans.projectid is not null
+		--			and trans.LkTransaction is not null
+		--	group by trans.TransId, trans.TransAmt)t
+		--	where t.bal = 0
 
-
+	end
 end
 go
 
